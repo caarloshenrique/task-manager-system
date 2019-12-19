229a61f2d708f3cfc728a7e70f54f38205e626ee
caarloshenrique <carlos.silva@novaandradina.org>
2019-12-18 22:30:15 -0200

    change style

--- project-manager-app/src/pages/Task/index.js
+++ project-manager-app/src/pages/Task/index.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 
-import { Container, TaskItem, ContainerTasks } from "./styles";
+import { Container, TaskItem, ContainerTasks, Teste, Title } from "./styles";
 import api from "../../services/api";
 
-export default function Task({ history }) {
+export default function Task() {
   const [task, setTask] = useState([]);
   useEffect(() => {
     const getTask = async () => {
@@ -17,16 +17,26 @@ export default function Task({ history }) {
     <div>
       <Container>
         <header>
-          <h1>Itens de Backlog:</h1>
+          <h1>Itens de Backlog</h1>
         </header>
         <ContainerTasks>
           {task.map(function(object) {
             return (
               <TaskItem key={object._id}>
-                <p>Nome: {object.name}</p>
+                <Teste>
+                  <i className="material-icons md-48">list_alt</i>
+                  <b>
+                    <Title>{object.name}</Title>
+                  </b>
+                </Teste>
+                <br></br>
                 <p>Descrição: {object.description}</p>
                 <p>Estimativa: {object.estimate}h</p>
-                <p>Autor: {object.author.name}</p>
+                <br></br>
+                <hr></hr>
+                <center>
+                  <p>Autor: {object.author.name}</p>
+                </center>
               </TaskItem>
             );
           })}
--- project-manager-app/src/pages/Task/styles.js
+++ project-manager-app/src/pages/Task/styles.js
@@ -28,18 +28,30 @@ export const TaskItem = styled.div`
   color: #0c3d5f;
   background: #fff;
   border-radius: 5px;
-  margin: 0 0 20px;
+  margin: 20px 12px;
   padding: 20px;
   width: 30%;
+  box-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.1);
 
   p {
-    font-size: 18px;
+    font-size: 15px;
   }
 `;
 
 export const ContainerTasks = styled.div`
   display: flex;
-  align-items: center;
   flex-wrap: wrap;
-  justify-content: space-between;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+export const Teste = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+export const Title = styled.p`
+  margin-left: 5px;
 `;
